aa7c732b01b9b3edceacd75ebdc92b8c07237e49
kime <khy970506@naver.com>
2019-03-25 18:48:41 +0900

    Add cpp file 5

---
+++ Chapter1_1/study2_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	bool bValue = false;
+	char chValue = 'A';
+	float fValue = 3.141592;
+	double dValue = 3.141592;
+
+	cout << fValue << endl;
+	return 0;
+}
